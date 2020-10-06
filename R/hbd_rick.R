5e1702d75668db60a55158e0d4b40710301476ae
Arun Seetharam <arnstrm@iastate.edu>
2020-10-06 13:11:58 -0500

    test

--- R/hbd_rick.R
+++ R/hbd_rick.R
@@ -1,17 +1,17 @@
 #' @description this function prints out birthday messages for Joel Le Forestier for his 26th birthday.
 #' @param from_who leaving it blank will randomly select a person; possible argument includes: name(s) of birthday message sender; "everyone"
 #' @return happy birthday message and the author of the message
-#' @examples hbd_joel(from_who = c("liz", "bethany"))
+#' @examples hbd_rick(from_who = c("liz", "bethany"))
 
-hbd_joel <- function(from_who) {
+hbd_rick <- function(from_who) {
 
   # joel's nicknames for people
-  nicknames = c("jacqueline" = "jackie",
-                "susan" = "sue",
-                "^corn\\D+" = "bethany",
-                "mariah" = "maria",
-                "^will\\D+" = "will",
-                "lpg" = "liz")
+  nicknames = c("Andrew" = "Andrew",
+                "Arun" = "Arun",
+                "Siva" = "Siva",
+                "Maryam" = "Maryam",
+                "Jennifer" = "Jennifer",
+                "Satheesh" = "Satheesh")
 
   # change the name to lower cases if capitalized & replace nicknames with real names
   from_who = stringr::str_replace_all(tolower(from_who), nicknames)
@@ -24,28 +24,28 @@ hbd_joel <- function(from_who) {
 
   suppressWarnings(
     if( length(from_who) == 1 & from_who == "everyone" ) {
-      from_who = hbdjoel::data$name
+      from_who = hbdrick::data$name
 
     } else if ( length(from_who) == 1 & from_who == "anyone" ) {
-      from_who = sample(hbdjoel::data$name, 1)
+      from_who = sample(hbdrick::data$name, 1)
 
-    # } else if ( any( from_who %in% hbdjoel::data$name == FALSE ) ) {
+    # } else if ( any( from_who %in% hbdrick::data$name == FALSE ) ) {
     } else {
 
       # find names of senders not in the data
-      unknown_senders = ifelse(from_who %in% c(hbdjoel::data$name, "matt"), NA, from_who)
+      unknown_senders = ifelse(from_who %in% c(hbdrick::data$name, "matt"), NA, from_who)
       unknown_senders = unknown_senders[!is.na(unknown_senders)] # drop NA
 
       cat("Who",
           paste(ifelse(length(unknown_senders) == 1, "is", "are")),
           paste(unknown_senders, collapse = ", "), "?\n")
 
-      from_who = from_who[which(from_who %in% c( hbdjoel::data$name, names(nicknames)))]
+      from_who = from_who[which(from_who %in% c( hbdrick::data$name, names(nicknames)))]
       }
 
   )
 
-  hbdjoel::data %>%
+  hbdrick::data %>%
     dplyr::filter(name %in% from_who) %>%
     dplyr::mutate(msg = glue::glue("{stringr::str_to_title(name)} says {msg}")) %>%
     .$msg
